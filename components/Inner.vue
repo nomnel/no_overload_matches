88ba0beea076cfefc2513f2fde1be3709b85fa67
nomnel <laxyz.nomnel@gmail.com>
2020-11-19 14:52:34 +0900

    reproduction

---
+++ components/Inner.vue
@@ -0,0 +1,11 @@
+<script lang="tsx">
+import Vue, { PropOptions } from 'vue'
+export default Vue.extend({
+  props: {
+    text: { required: true, type: String } as PropOptions<string>,
+  },
+  render() {
+    return <div>{this.text}</div>
+  },
+})
+</script>
---
+++ components/Wrapper.vue
@@ -0,0 +1,14 @@
+<script lang="tsx">
+import Vue from 'vue'
+import Inner from '~/components/Inner.vue'
+
+export default Vue.extend({
+  render() {
+    return (
+      <div>
+        <Inner text="hoge"></Inner>
+      </div>
+    )
+  },
+})
+</script>
